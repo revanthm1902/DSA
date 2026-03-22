b15342d12575291bb42cc7e180a2091ce6842050
revanth <revanthm051@gmail.com>
2026-03-23 02:27:03 +0530

    Add pre-order traversal method to binary tree implementation

--- Tree/tree.java
+++ Tree/tree.java
@@ -21,10 +21,16 @@ public class tree {
             return newNode;
         }
     }
+    public static void preOrder(Node root){ // PreOrder Traversal
+        if(root==null) return;
+        System.out.print(root.data+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
     public static void main(String[] args){
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree=new BinaryTree();
         Node root=tree.buildTree(nodes);
-        System.out.println(root.data);
+        preOrder(root);
     }
 }
