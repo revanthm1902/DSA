0bf95ad9019b020dbcf674c22c05c33cffc8a922
revanth <revanthm051@gmail.com>
2026-05-03 22:12:02 +0530

    Add hashmap implementation and update launch configurations

---
+++ STSII/hashmap.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class hashmap {
+    public static void main(String[] args){
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "one");
+        map.put(2, "two");
+        map.put(3, "three");
+
+        System.out.println(map.get(2)); // Output: two
+        System.out.println(map.containsKey(3)); // Output: true
+        System.out.println(map.containsValue("four")); // Output: false
+        map.remove(1);
+        System.out.println(map.get(1)); // Output: null
+        System.out.println(map.size()); // Output: 2
+    }
+}
