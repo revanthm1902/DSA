7dcc6bf5bcb96b2576a573503be1ae7a777693f7
revanth <revanthm051@gmail.com>
2026-05-04 06:55:28 +0530

    Remove commented-out legacy implementation of maxSlidingWindow method

--- STS2/maxSlidingWindow.java
+++ STS2/maxSlidingWindow.java
@@ -27,3 +27,18 @@ public class maxSlidingWindow {
         for(int val:res) System.out.print(val+" ");
     }
 }
+
+
+// static int[] msw(int[] nums, int k){
+//     int n = nums.length;
+//     int[] res = new int[n - k + 1];
+
+//     for(int i = 0; i <= n - k; i++){
+//         int max = nums[i];
+//         for(int j = i; j < i + k; j++){
+//             max = Math.max(max, nums[j]);
+//         }
+//         res[i] = max;
+//     }
+//     return res;
+// }   
